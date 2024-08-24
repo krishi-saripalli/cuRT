f61ab39dbc7dc9b2b477eaff60033ba90b8f7205
Krishi Saripalli <krishi_saripalli@brown.edu>
2024-08-23 21:31:04 -0700

    removed

--- build/CMakeFiles/raymarcher.dir/raymarcher_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/raymarcher.dir/raymarcher_autogen/mocs_compilation.cpp.o: \
-  /Users/saripallikrishi/projects/raymarcher/build/raymarcher_autogen/mocs_compilation.cpp
--- build/CMakeFiles/raymarcher_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/raymarcher_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/raymarcher_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
