0bf31908de8e81c26eab535bcc1fe716a02f4619
krishi-saripalli <krishi_saripalli@brown.edu>
2024-11-25 00:52:28 -0500

    point light working

--- kernel/light.cu
+++ kernel/light.cu
@@ -11,6 +11,7 @@ __device__ vec4 illumination(const GPURenderData& renderData, const Hit& hit) {
                 total += directionalIllumination(renderData,hit,light);
                 break;
             case GPULightType::LIGHT_POINT:
+                total += pointIllumination(renderData,hit,light);
                 break;
             case GPULightType::LIGHT_SPOT:
                 break;
@@ -38,18 +39,52 @@ __device__ vec4 directionalIllumination(const GPURenderData& renderData, const H
     float ks = renderData.globalData.ks;
     float n = hit.shape->primitive.material.shininess;
     vec4 os = hit.shape->primitive.material.cSpecular;
-    // Normalize vectors
-    vec4 v = -hit.direction;  // View vector
+    vec4 v = -hit.direction;  
     v.normalize();
 
-    // Calculate reflection vector (using negated light direction to match li)
     vec4 ri = (-li) - (2.f * dot(-li, hit.normal) * hit.normal);
     ri.normalize();
 
     vec4 specular = ks * powf(max(0.0f, dot(ri, v)), n) * os;
-    //print(hit.direction);
 
 
     return ambient + (intensity * (diffuse + specular));
   
 }
+
+__device__ vec4 pointIllumination(const GPURenderData& renderData, const Hit& hit, const GPUSceneLightData& light) {
+    float ka = renderData.globalData.ka;
+    vec4 oa = hit.shape->primitive.material.cAmbient;
+
+    vec4 ambient = ka * oa;
+
+    vec4 intensity = light.color;
+    float kd = renderData.globalData.kd;
+    vec4 od = hit.shape->primitive.material.cDiffuse;
+    vec4 li = light.pos - hit.intersection;
+    li.normalize(); // TODO: do this once on CPU, not here
+    vec4 diffuse = kd * od * max(0.0f, dot(hit.normal,li));
+
+    
+    float ks = renderData.globalData.ks;
+    float n = hit.shape->primitive.material.shininess;
+    vec4 os = hit.shape->primitive.material.cSpecular;
+    vec4 v = -hit.direction; 
+    v.normalize();
+
+    vec4 ri = (-li) - (2.f * dot(-li, hit.normal) * hit.normal);
+    ri.normalize();
+
+    vec4 specular = ks * powf(max(0.0f, dot(ri, v)), n) * os;
+
+    //attenuation
+    vec3 c = light.function;
+    float distance = li.length();
+    float att = fmin(1.f, 1.f/(c[0] + (distance*c[1]) + (distance*distance*c[2])));
+
+
+    return ambient + ( att * intensity * (diffuse + specular));
+  
+}
+
+
